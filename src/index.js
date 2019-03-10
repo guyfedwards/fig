391b263b3e7a15acfe187c3110d585ba6800b9dc
guyfedwards <guyfedwards@gmail.com>
2019-03-10 19:11:11 +0000

    expose get

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const add = require('./add')
 const copy = require('./copy')
+const get = require('./get')
 const list = require('./list')
 const open = require('./open')
 
 module.exports = {
   add,
   copy,
+  get,
   list,
   open,
 }
